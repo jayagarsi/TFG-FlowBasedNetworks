087f684b5a94935bab5dedd02f4adc3b19ca54f6
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-01-16 22:25:33 +0100

    diverses proves pel dynamics

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 int main() {
     int n = 4;
-    int k = 2;
+    int k = 3;
     Network G(n, k);
-    // Vertex 0 == v, 1 == x, 2 == y, z == 3
-    string model = "avg";
-    G.buyEdge(0, 1, 1);
+    string model = "min";
+    // Vertex a == 0, b == 1, c == 2, d == 3, e == 4, 
+    G.buyEdge(0, 1, 2);
     G.buyEdge(0, 3, 1);
     G.buyEdge(1, 0, 1);
-    G.buyEdge(1, 3, 1);
-    G.buyEdge(2, 1, 1);
-    G.buyEdge(2, 3, 1);
-    G.buyEdge(3, 2, 2); 
+    G.buyEdge(1, 2, 2);
+    G.buyEdge(2, 0, 3);
+    G.buyEdge(3, 0, 3);
     cout << "Original Graph" << endl;
     G.printAdjacencyMatrix(0);
-    G.printModelsUtility("all");
+    G.printModelsUtility(model);
     
-
     //vector<int> agentStrategy = G.agentBestResponse(0, model);
     G.simulateGameDynamics(model);
     cout << "-------------------------------------" << endl;
     cout << "-------------------------------------" << endl;
     cout << "-------------------------------------" << endl;
     cout << "Graph changed by agents best response" << endl;
-    //vector<int> bestStrategy = G.agentBestResponse(0, model);
-    //for (int i = 0; i < bestStrategy.size(); ++i)
-    //    cout << bestStrategy[i] << endl;
     G.printAdjacencyMatrix(0);
-    G.printModelsUtility("all");
+    G.printModelsUtility(model);
 
+    /*
+    vector<int> bestStrategy = G.agentBestResponse(0, model);
+    for (int i = 0; i < bestStrategy.size(); ++i)
+        cout << bestStrategy[i] << endl;
+    */
 }

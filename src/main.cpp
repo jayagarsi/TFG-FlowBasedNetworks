cd9b6cc0a9e10cbf9ab5f716e3b76d5c93c06307
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-01-30 20:58:23 +0100

    el main va canviant per l'entrada així que aquest commit la sua

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,27 @@
 using namespace std;
 
 int main() {
-    int n = 4;
-    int k = 3;
+    int n = 6;
+    int k = 2;
     Network G(n, k);
     string model = "min";
-    // Vertex a == 0, b == 1, c == 2, d == 3, e == 4, 
     G.buyEdge(0, 1, 2);
-    G.buyEdge(0, 3, 1);
     G.buyEdge(1, 0, 1);
-    G.buyEdge(1, 2, 2);
-    G.buyEdge(2, 0, 3);
-    G.buyEdge(3, 0, 3);
-    cout << "Original Graph" << endl;
-    G.printAdjacencyMatrix(0);
-    G.printModelsUtility(model);
-    
-    //vector<int> agentStrategy = G.agentBestResponse(0, model);
-    G.simulateGameDynamics(model);
-    cout << "-------------------------------------" << endl;
-    cout << "-------------------------------------" << endl;
-    cout << "-------------------------------------" << endl;
-    cout << "Graph changed by agents best response" << endl;
-    G.printAdjacencyMatrix(0);
-    G.printModelsUtility(model);
+    G.buyEdge(1, 3, 1);
+    G.buyEdge(2, 1, 2);
+    G.buyEdge(3, 4, 2);
+    G.buyEdge(4, 2, 1);
+    G.buyEdge(4, 3, 1);
+    G.buyEdge(5, 4, 2);
 
-    /*
+    G.simulateGameDynamics(model);
+    /*G.printAdjacencyMatrix(0);
+    G.printModelsUtility("min");
     vector<int> bestStrategy = G.agentBestResponse(0, model);
-    for (int i = 0; i < bestStrategy.size(); ++i)
+    /*for (int i = 0; i < bestStrategy.size(); ++i)
         cout << bestStrategy[i] << endl;
+    G.setAgentStrategy(0, bestStrategy);
+    G.printModelsUtility("min");
+    G.printAdjacencyMatrix(0);
     */
 }

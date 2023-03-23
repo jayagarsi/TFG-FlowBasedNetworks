1a874a792418a5c5afb0f00785f2aef74d0f5f04
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-03-23 18:45:32 +0100

    made a random graph generator using nx

--- src/randomGenerator.py
+++ src/randomGenerator.py
@@ -1,23 +1,49 @@
 import networkx as nx
-import my_draw_networx_edge_labels as my_nx
-import matplotlib.pyplot as plt
+import random as rd
 
 if __name__ == '__main__':
-    file = open("graphInstance.txt", "r")
+    
+    ### Read parameters of the graph
+    file = open("randomGraphInstance.txt", "r")
     n = int(file.readline())
-    adjacencyMatrix = []
-    for i in range(n):
-        row = list(file.readline())
-        row = [int(w) for w in row[:-1]]
-        #print(row)
-        adjacencyMatrix.append(row)
+    m = int(file.readline())
+    k = int(file.readline())
+    s = int(file.readline())
+    p = int(file.readline())
+    type = file.readline()[:-1]
+
     file.close()
 
-    #print(adjacencyMatrix)
-
     G = nx.DiGraph()
+    ### Generate different types of graph based on the input
+    if type == "sparse":
+        G = nx.gnm_random_graph(n, m, s, True)
+    elif type == "dense":
+        G = nx.dense_gnm_random_graph(n, m, s, True)
+    elif type == "gnp":
+        G = nx.fast_gnp_random_graph(n, p, s, True)
+
+    ### Generate the adjacency matrix
+    adjacencyMatrix = [[0 for i in range(n)] for j in range(n)]
+    for u, v, in G.edges():
+        adjacencyMatrix[u][v] = 1
+
+    ### Assign to each existing edge a random value between 1 and available budget
+    rd.seed(s)      # uncomment this for repeating the experiments
     for i in range(n):
+        availableBudget = k
         for j in range(n):
-            edgeWeight = adjacencyMatrix[i][j]
-            if (edgeWeight != 0):
-                G.add_edge(i, j, weight = edgeWeight)
+            if (adjacencyMatrix[i][j] >= 1):
+                if (availableBudget > 1):
+                    selectedWeight = rd.randint(1, availableBudget)
+                    adjacencyMatrix[i][j] = selectedWeight
+                    availableBudget -= selectedWeight
+                elif (availableBudget == 1):
+                    adjacencyMatrix[i][j] = 1
+                    availableBudget -= 1
+                else:
+                    adjacencyMatrix[i][j] = 0
+    
+    file = open("randomGraphInstance.txt", "w")
+    for row in adjacencyMatrix:
+        file.write(' '.join(str(elem) for elem in row) + '\n')

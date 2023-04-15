c34a5733fc7b9e556f5eb37e40518fe44544af03
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-04-15 17:27:23 +0200

    canviat la generacio d'erdos

--- src/randomGenerator.py
+++ src/randomGenerator.py
@@ -9,19 +9,17 @@ if __name__ == '__main__':
     m = int(file.readline())
     k = int(file.readline())
     s = int(file.readline())
-    p = int(file.readline())
+    p = float(file.readline())
     type = file.readline()[:-1]
 
     file.close()
 
     G = nx.DiGraph()
     ### Generate different types of graph based on the input
-    if type == "sparse":
+    if type == "gnm":
         G = nx.gnm_random_graph(n, m, s, True)
-    elif type == "dense":
-        G = nx.dense_gnm_random_graph(n, m, s, True)
-    elif type == "gnp":
-        G = nx.fast_gnp_random_graph(n, p, s, True)
+    elif type == "erdos":
+        G = nx.erdos_renyi_graph(n, p, s, True)
 
     ### Generate the adjacency matrix
     adjacencyMatrix = [[0 for i in range(n)] for j in range(n)]
@@ -29,18 +27,18 @@ if __name__ == '__main__':
         adjacencyMatrix[u][v] = 1
 
     ### Assign to each existing edge a random value between 1 and available budget
-    rd.seed(s)      # uncomment this for repeating the experiments
+    rd.seed(s)      # this allows the experiments to be repeated for same seeds
     for i in range(n):
         availableBudget = k
         for j in range(n):
-            if (adjacencyMatrix[i][j] >= 1):
+            if (adjacencyMatrix[i][j] > 0):
                 if (availableBudget > 1):
                     selectedWeight = rd.randint(1, availableBudget)
                     adjacencyMatrix[i][j] = selectedWeight
                     availableBudget -= selectedWeight
                 elif (availableBudget == 1):
                     adjacencyMatrix[i][j] = 1
-                    availableBudget -= 1
+                    availableBudget = 0
                 else:
                     adjacencyMatrix[i][j] = 0
     

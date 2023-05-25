12e48235f12c2df8adf2e1845116625493296a86
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-05-25 18:09:54 +0200

    made automatization of experiment 4 reducion of network

--- src/experiment4-reducingNetwork.sh
+++ src/experiment4-reducingNetwork.sh
@@ -1,55 +1,162 @@
 #!/bin/bash
 
 runMultipleExperiments() {
+    $n $k $m $p $s $startingAgents $stepSize $maximumReduction $graphType $destinyFilePath
     n=$1
-    m=$2
-    p=$3
-    maxK=$4
+    k=$2
+    m=$3
+    p=$4
     s=$5
-    graphType=$6
-    order=$7
-    direction=$8
-    filePath=$9
-    dynamicsImageFile="./dynamicsGraph.png"
-    agentsToAdd=0
-    agentsToRemove=0
-    modification=""
+    startingAgents=$6
+    stepSize=$7
+    maximumReduction=$8
+    graphType=$9
+    filePath=${10}
+    parametersFile="./dynamicsParameters.txt"
+    modification="exp"
+    orders="ra pr"
 
-    # Min-Flow model
-    model="min"
-    csvFile="$model""Flow-n$n-m$m-s$s.csv"
-    touch $csvFile
-    echo "OriginalUtility,EquilibriumUtility,Nrounds,NedgesDir,NedgesUndir,MaxClust" > $csvFile
-    for ((k=1; k<=maxK; k++)) do
-        ./dynamics "$n $m $k $s $p $model $graphType $order $modification $agentsToAdd $csvFile"
-        mv $dynamicsImageFile $filePath
-    done
-    rm -f $csvFile
+    for order in $orders
+    do
+        # Min-Flow model
+        model="min"
+        case $graphType in
+            gnp)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n-p$p-s$s.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n-p$p-s$s.csv"
+                ;;
+            gnm)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n-m$m-s$s.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n-m$m-s$s.csv"
+                ;;
+            empty)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n.csv"
+                ;;
+            *)
+                echo "Something went wrong when assigning csvFile"
+                ;;
+        esac
+        touch $csvFile
+        echo "OriginalUtility,EquilibriumUtility,Nrounds,NedgesDir,NedgesUndir,MaxClust,chgOriginalUtility,chgEquilibriumUtility,chgNrounds,chgNedgesDir,chgNedgesUndir,chgMaxClust" > $csvFile
+        for ((agentsToRemove=startingAgents; agentsToRemove<=maximumReduction; agentsToRemove-=stepSize)) do
+            echo "--------- EXPERIMENT WHEN REMOVING $agentsToRemove agents ---------"
+            originalDynamicsImageFile="originalDynamics-"$prder"-"$model"Flow"
+            reducedDynamicsImageFile="reducedDynamics-"$order"-"$model"Flow-n"$agentsToAdd
+            echo $n > echo $k > echo $m > echo $p > echo $s > echo $model > echo $graphType > echo $order > echo $modification > echo $csvFile > echo $experiment > echo > $agentsToRemove > $parametersFile
+            if ./dynamics; then
+                echo "Dynamics was executed correctly!"
+            else
+                echo "Someting went wrong executing the Dynamics"
+                echo ",,,,," > $csvFile
+            fi
+            case $order in
+                ra)
+                    mv $originalDynamicsImageFile $filePath
+                    mv $reducedDynamicsImageFile $filePath"/Random/"
+                    ;;
+                rr)
+                    mv $originalDynamicsImageFile $filePath
+                    mv $reducedDynamicsImageFile $filePath"/RoundRobin/"
+                    ;;
+                *)
+                    echo "Something went wrong"
+                    ;;
+            esac
+        done
+        mv -vn $csvFile $filePath
+        rm -f $parametersFile
+    
 
-    # Avg-Flow model
-    model="avg"
-    csvFile="$model""Flow-n$n-m$m-s$s"
-    touch $csvFile
-    echo "OriginalUtility,EquilibriumUtility,Nrounds,NedgesDir,NedgesUndir,MaxClust" > $csvFile
-    for ((k=1; k<=maxK; k++)) do
-        fileName="avgFlow-$order-k$k.png"
-        ./dynamics "$n $m $k $s $p $model $graphType $modification $agentsToAdd $csvFile"
-        mv $dynamicsImageFile $filePath
+        # Avg-Flow model
+        model="avg"
+        case $graphType in
+            gnp)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n-p$p-s$s.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n-p$p-s$s.csv"
+                ;;
+            gnm)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n-m$m-s$s.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n-m$m-s$s.csv"
+                ;;
+            empty)
+                csvFileOriginalDynamics="originalDynamics-$model""Flow-$order-n$n.csv"
+                csvFileExpandedDynamics="expandedDynamics-$model""Flow-$order-n$n.csv"
+                ;;
+            *)
+                echo "Something went wrong when assigning csvFile"
+                ;;
+        esac
+        touch $csvFile
+        echo "OriginalUtility,EquilibriumUtility,Nrounds,NedgesDir,NedgesUndir,MaxClust,chgOriginalUtility,chgEquilibriumUtility,chgNrounds,chgNedgesDir,chgNedgesUndir,chgMaxClust" > $csvFile
+        for ((agentsToRemove=startingAgents; agentsToRemove<=maximumExpansion; agentsToRemove-=stepSize)) do
+            echo "--------- EXPERIMENT WHEN REMOVING $agentsToRemove agents ---------"
+            originalDynamicsImageFile="originalDynamics-"$prder"-"$model"Flow"
+            reducedDynamicsImageFile="reducedDynamics-"$order"-"$model"Flow-n"$agentsToAdd
+            echo $n > echo $k > echo $m > echo $p > echo $s > echo $model > echo $graphType > echo $order > echo $modification > echo $csvFile > echo $experiment > echo > $agentsToAdd  > $parametersFile
+            if ./dynamics; then
+                echo "Dynamics was executed correctly!"
+            else
+                echo "Someting went wrong executing the Dynamics"
+                echo ",,,,," > $csvFile
+            fi
+            case $order in
+                ra)
+                    mv $originalDynamicsImageFile $filePath
+                    mv $reducedDynamicsImageFile $filePath"/Random/"
+                    ;;
+                rr)
+                    mv $originalDynamicsImageFile $filePath
+                    mv $reducedDynamicsImageFile $filePath"/RoundRobin/"
+                    ;;
+                *)
+                    echo "Something went wrong"
+                    ;;
+            esac
+        done
+        mv -vn "originalGraph.png" $filePath
+        mv -vn $csvFile $filePath
+        rm -f $parametersFile
     done
-    rm $csvFile
+    echo "---------------------------------------------"
+    echo "--------- FINISHED ALL EXPERIMENTS ----------"
+    echo "---------------------------------------------"
 }
 
 n=10
+k=2
 m=8
-maxK=12
-maxN=1
-s=200
-agentsToAdd=1
 p=0.5
+s=200
+startingAgents=5
+stepSize=5
+maximumReduction=25
 graphType="gnm"
-order="ra"
-direction=1
-experiment="iKfN"
-destinyFilePath="../test/randomNetworks/FixedNIncreasingK/Gnm/Graph2-n10-m8-s200/Random"
+case $graphType in
+    gnp)
+        basePath="../test/NetworkReduction/Gnp"
+        mkdir -p $basePath
+        destinyFilePath=$basePath"/Graph2-n$n-p$p-s$s"
+        ;;
+    gnm)
+        basePath="../test/NetworkReduction/Gnm"
+        mkdir -p $basePath
+        destinyFilePath=$basePath"/Graph2-n$n-m$m-s$s"
+        ;;
+    empty)
+        basePath="../test/NetworkReduction/emptyGraph"
+        mkdir -p $basePath
+        destinyFilePath=$basePath"/Graph2-n$n-s$s"
+        ;;
+    *)
+        echo "Pick a correct graph type"
+        return
+        ;;
+esac
+
+mkdir -p $destinyFilePath
+mkdir -p $destinyFilePath"/Random"
+mkdir -p $destinyFilePath"/RoundRobin"
 make
-runMultipleExperiments $n $m $p $maxK $s $graphType $order $direction $destinyFilePath
+runMultipleExperiments $n $k $m $p $s $startingAgents $stepSize $maximumReduction $graphType $destinyFilePath
+make clearSpace

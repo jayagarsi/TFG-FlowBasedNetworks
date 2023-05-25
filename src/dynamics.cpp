46f0cca03c2b2ae90e962cd119d41cd7a22a4aa3
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2023-05-25 18:06:27 +0200

    added treatment for expansion or reduction of NE networks experiment

--- src/dynamics.cpp
+++ src/dynamics.cpp
@@ -11,7 +11,7 @@ int sumValues(const vector<int> &array) {
 }
 
 int main() {
-    int n, m, k, s;
+    int n, m, k, s, agentsToChange;
     int direction = 0;
     double p;
     string model, graphType, order, modification, csvFilename, experiment;
@@ -19,11 +19,10 @@ int main() {
     ifstream params;
     params.open("./dynamicsParameters.txt", ios::in);
     params >> n >> k >> m >> p >> s >> model >> graphType >> order >> modification >> csvFilename >> experiment;
-
+    if (modification == "exp" or modification == "red") params >> agentsToChange;
     Network G(n, k);
     if (graphType == "gnm" or graphType == "gnp") 
         G.buildRandomGraph(n, m, k, s, p, graphType);
-    G.buildRandomGraph(n, m, k, s, p, graphType);
     if (experiment == "fixedNincreasingK") name = name + "-k" + to_string(k);
     else if (experiment == "fixedKincreasingN") name = name + "-n" + to_string(n);
     G.drawGraph(direction, name);
@@ -59,14 +58,67 @@ int main() {
 
     if (experiment == "fixedNincreasingK") name = model + "Flow-" + order + "-k" + to_string(k);
     else if (experiment == "fixedKincreasingN") name = model + "Flow-" + order + "-n" + to_string(n);
-    else if (experiment == "networkExpansion") {
+    else if (experiment == "networkExpansion" or experiment == "networkReduction") { 
+        name = "originalDynamics-" + order + "-" + model + "Flow";
+        G.drawGraph(direction, name);
+        int newN = n;
+        if (experiment == "networkExpansion") {
+            newN += agentsToChange;
+            G.addNewAgents(agentsToChange);
+            name = "expandedDynamics-";
+        }
+        else if (experiment == "networkReduction") {
+            newN -= agentsToChange;
+            G.removeAgents(agentsToChange);
+            name = "reducedDynamics-";
+        }
 
+        double changedOriginalUtility, changedEquilibriumUtility;
+        if (model == "min")
+            changedOriginalUtility = G.minFlowSocialUtility();
+        else if (model == "avg")
+            changedOriginalUtility = G.avgFlowSocialUtility();
+
+        int changedNumberOfRounds = 0;
+        if (order == "ra") changedNumberOfRounds = G.simulateGameDynamicsRandomOrder(model, s);
+        else if (order == "rr") changedNumberOfRounds = G.simulateGameDynamics(model);
+        else if (order == "pr") {
+            vector<int> agentOrder(n);
+            for (int i = 0; i < n; ++i)
+                agentOrder[i] = n-i-1;
+            changedNumberOfRounds = G.simulateGameDynamics(model, agentOrder);
+        }
+        if (model == "min")
+            changedEquilibriumUtility = G.minFlowSocialUtility();
+        else if (model == "avg")
+            changedEquilibriumUtility = G.avgFlowSocialUtility();
+
+        vector<int> changedEdgesDirected = G.numberOfEdges(0);
+        vector<int> changedEdgesUndirected = G.numberOfEdges(1);
+
+        int changedNumEdgesDirected = sumValues(changedEdgesDirected);
+        int changedNumEdgesUndirected = sumValues(changedEdgesUndirected);
+
+        vector<int> changedMaximalCluster = G.computeMaximalCluster(k + 1);
+        int changedMaximalClusterSize = changedMaximalCluster.size();
+
+        name += order + "-" + model + "Flow-n" + to_string(agentsToChange);
+        G.drawGraph(direction, name);
+
+        ofstream file;
+        string path = "./" + csvFilename;
+        file.open(path, ios::out | ios::app);
+        file << originalUtility << "," << equilibriumUtility << "," << numberOfRounds << "," << numEdgesDirected << "," << numEdgesUndirected << "," << maximalClusterSize;
+        file << changedOriginalUtility << "," << changedEquilibriumUtility << "," << changedNumberOfRounds << "," << changedNumEdgesDirected << "," << changedNumEdgesUndirected << "," << changedMaximalClusterSize << endl;
+        file.close();
     }
-    G.drawGraph(direction, name);
+
     // Append results to file
-    ofstream file;
-    string path = "./" + csvFilename;
-    file.open(path, ios::out | ios::app);
-    file << originalUtility << "," << equilibriumUtility << "," << numberOfRounds << "," << numEdgesDirected << "," << numEdgesUndirected << "," << maximalClusterSize << endl;
-    file.close();
+    if (experiment == "fixedNincreasingK" or experiment == "fixedKincreasingN") {
+        ofstream file;
+        string path = "./" + csvFilename;
+        file.open(path, ios::out | ios::app);
+        file << originalUtility << "," << equilibriumUtility << "," << numberOfRounds << "," << numEdgesDirected << "," << numEdgesUndirected << "," << maximalClusterSize << endl;
+        file.close();
+    }
 }
